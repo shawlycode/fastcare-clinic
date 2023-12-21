6716f54f92e23d2df93ea9cbaff82045a9a2f8b1
Shawlycode <kutinaorlando@gmail.com>
2023-12-21 18:11:12 +0000

    added btns

--- src/pages/landingpage/LandingPage.jsx
+++ src/pages/landingpage/LandingPage.jsx
@@ -1,16 +1,36 @@
 import './landingpage.css'
-import Login from '../login/Login'
+import { useNavigate } from 'react-router-dom';
+import floids from '../../floids.gif'
 
 const LandingPage = () => {
+
+
+  const navigate = useNavigate();
+  const handleLogin = (e) => {
+    e.preventDefault()
+    navigate('/login')
+
+  }
   return (
-    <div className='landing__page'>
-      <div className="left__container">
-        <h1>Welcome to Fastcare clinic</h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet a sunt in deserunt rerum nemo porro est et ab tenetur, aliquid quibusdam doloremque consequuntur voluptatem, placeat, modi suscipit ad laboriosam.
-          Quibusdam ipsa vel officia quas facere </p>
-      </div>
-      <div className="right__container">
-        <Login />
+    <div className="container">
+      <div className='landing__page'>
+        <div className="left__container">
+          <h1>Welcome to Fastcare clinic</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet a sunt in deserunt rerum nemo porro est et ab tenetur, aliquid quibusdam doloremque consequuntur voluptatem, placeat, modi suscipit ad laboriosam.
+            Quibusdam ipsa vel officia quas facere </p>
+        </div>
+        <div className="right__container">
+          <div className="header__right-profile">
+            {/* <video src={video} loop={Infinity} autoPlay={true} width="100%" controls={false}
+            /> */}
+            <img src={floids} alt="" />
+
+          </div>
+        </div>
+        <div className="btn__container">
+          <button type="button" class="btn btn-warning" onClick={handleLogin}>Login</button>
+          <button type="button" class="btn btn-warning">Register</button>
+        </div>
       </div>
     </div>
   )
