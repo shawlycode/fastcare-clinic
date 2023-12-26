01e2205c4f7f3b8274e5ddf79192c36f73657d80
Shawlycode <kutinaorlando@gmail.com>
2023-12-26 09:59:24 +0000

    added routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import './App.css';
-import Sidebar from './components/sidebar/Sidebar';
-import LandingPage from '../src/pages/landingpage/LandingPage'
-import { Routes, Route } from 'react-router-dom';
-// import Overview from './pages/Overview';
-// import { Reports, ReportsOne, ReportsTwo, ReportsThree } from './pages/Reports';
-// import Team from './pages/Team';
-function App() {
+import { Route, Routes } from "react-router-dom"
+import Login from "./pages/login/Login"
+import LandingPage from './pages/landingpage/LandingPage'
+import Sidebar from './components/sidebar/Sidebar'
+const App = () => {
   return (
-    <>
-      <Sidebar />
-      <Routes>
-        {/* <Route path='/landingpage' exact component={<LandingPage />} /> */}
-        {/* <Route path='/reports' exact component={Reports} />
-        <Route path='/reports/reports1' exact component={ReportsOne} />
-        <Route path='/reports/reports2' exact component={ReportsTwo} />
-        <Route path='/reports/reports3' exact component={ReportsThree} />
-        <Route path='/team' exact component={Team} /> */}
-      </Routes>
-    </>
-  );
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/sidebar" element={<Sidebar />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
